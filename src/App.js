a85628a386abd3347e592ec990417089074cd967
santiagoDeMarco <corvalansantiago29@gmail.com>
2023-08-11 13:32:43 -0300

    components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage"
+import Contact from "./components/Contact";
 
-export default function App() {
-  return "App";
+function App() {
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="" element={<HomePage />} />
+        <Route path="/contact" element= {<Contact/>} />
+			</Routes>
+		</div>
+	);
 }
+
+export default App;
